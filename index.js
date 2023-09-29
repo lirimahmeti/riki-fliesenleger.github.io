77334c6c252390c7d5bf80d5a1b2dca034888ccf
Lirim Ahmeti <lirimiii2010@gmail.com>
2023-09-29 13:16:33 +0200

    shtova sliderin

---
+++ index.js
@@ -0,0 +1,46 @@
+const slider = document.querySelector(".slider1")
+const innerslider = document.querySelector(".slider-inner")
+
+let pressed = false;
+let startX;
+let x;
+
+slider.addEventListener("mousedown", (e)=>{
+    pressed = true;
+    startX = e.offsetX - innerslider.offsetLeft;
+    slider.style.cursor = "grabbing"
+    console.log(innerslider.offsetLeft);
+});
+
+slider.addEventListener('mouseenter', ()=>{
+    slider.style.cursor = "grab"
+})
+
+slider.addEventListener('mouseup', ()=>{
+    slider.style.cursor = "grab"
+})
+
+window.addEventListener('mouseup', ()=>{
+    pressed = false;
+})
+
+slider.addEventListener('mousemove', (e)=>{
+    if(!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX;
+
+    innerslider.style.left = `${x - startX}px`;
+    checkBoundary()
+})
+
+function checkBoundary(){
+    let outer = slider.getBoundingClientRect();
+    let inner = innerslider.getBoundingClientRect();
+
+    if(parseInt(innerslider.style.left) > 0){
+        innerslider.style.left = "0px"
+    } else if(inner.right < outer.right){
+        innerslider.style.left = `-${inner.width - outer.width}px`
+    }
+}
